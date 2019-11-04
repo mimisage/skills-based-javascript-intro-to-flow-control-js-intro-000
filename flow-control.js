1cef0c1147b54f8436d5cba6093c77305688db83
Michelle Sage <michelle2sage@gmail.com>
2019-11-04 04:58:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,10 +6,14 @@ function basicTeenager(age) {
 function teenager(age) {
   if (age >=13 && age <=19) {
   return "You are a teenager!"}
+  else (age >=12 || age <=20){
+  return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >=13 && age <=19) {
+  return "You are a teenager!"}
 }
 
 function ternaryTeenager(age) {
